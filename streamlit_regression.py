a4c17b3231eaf567cf037c80968971b417ea8409
chandanjha2feb <chandanjha2feb@gmail.com>
2025-05-11 14:54:48 +0200

    ANN Regression

---
+++ streamlit_regression.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pickle
+import tensorflow as tf
+
+# Load the model
+model = tf.keras.models.load_model("regression_model.h5")
+
+## load the encoder and scalar
+with open("regression_one_hot_encoder_geography.pkl", "rb") as file:
+    label_encoder_geo=pickle.load(file)
+
+with open("regression_label_encoder_gender.pkl", "rb") as file:
+    label_encoder_gender=pickle.load(file)
+
+with open("regression_scalar.pkl", "rb") as file:
+    scaler=pickle.load(file)
+
+## streamlit app
+st.title(" Estimatedd Salary Prediction")
+
+# input data
+geography=st.selectbox("Geography", label_encoder_geo.categories_[0])
+gender=st.selectbox("Gender", label_encoder_gender.classes_)
+age=st.slider("Age", 18, 92)
+balance=st.number_input("Balance")
+credit_score=st.number_input("Credit Score")
+tenure=st.slider("Tenure",  0, 10)
+num_of_products=st.slider("Number of Products", 1, 4)
+has_cr_card=st.selectbox("Has credit card", [0,1])
+is_active_member=st.selectbox("Is Active Member", [0,1])
+exited=st.selectbox("Exited", [0,1])
+
+## Prepare a dictionary
+input_data=pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'Exited': [exited]
+})
+
+# One-hot encode 'Geography'
+geo_encoded=label_encoder_geo.transform([[geography]])
+geo_encoded_df=pd.DataFrame(geo_encoded, columns=label_encoder_geo.get_feature_names_out(["Geography"]))
+
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+# Scale the data
+input_data_scaled=scaler.transform(input_data)
+
+## Predict the churn
+prediction=model.predict(input_data_scaled)
+prediction_prob=prediction[0][0]
+st.write(f"Predicted Salary: {prediction_prob:.2f}")
+
+if prediction_prob >  0.5:
+    st.write("The customer is likely to churn")
+else:
+    st.write("The customer is not likely to churn")
